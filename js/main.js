b5e6b7cb357e3779c33b518fb44344cbecc474d1
QUGOR <93441911+Matatabe23@users.noreply.github.com>
2023-05-24 16:40:49 +0300

    1

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// const video = document.querySelector('.video-bg');
+// if (window.innerWidth <= 600) {
+//     video.querySelector('source').setAttribute('src', 'video/kokushibo.mp4')
+//     // Также можно изменить значение атрибута type на соответствующий тип для мобильных устройств
+// } else {
+//     video.querySelector('source').setAttribute('src', 'video/kokushibou2.mp4')
+//     // Также можно изменить значение атрибута type на соответствующий тип для настольных компьютеров
+// }
